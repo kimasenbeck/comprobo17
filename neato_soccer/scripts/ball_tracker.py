c567c28f8d81612bccb30cdbf5fc042630c314dc
Kim Asenbeck <kasenbec@wellesley.edu>
2017-02-28 07:31:21 -0500

    neato soccer complete!

--- neato_soccer/scripts/ball_tracker.py
+++ neato_soccer/scripts/ball_tracker.py
@@ -22,8 +22,10 @@ class BallTracker(object):
         rospy.init_node('ball_tracker')
         self.cv_image = None                        # the latest image from the camera
         self.bridge = CvBridge()                    # used to convert ROS messages to OpenCV
-
+        self.twist = None
+        
         rospy.Subscriber(image_topic, Image, self.process_image)
+        rospy.Subscriber("bump", Image, self.process_bump)
         self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
         cv2.namedWindow('video_window')
 
@@ -31,17 +33,45 @@ class BallTracker(object):
         """ Process image messages from ROS and stash them in an attribute
             called cv_image for subsequent processing """
         self.cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+        self.binary_image = cv2.inRange(self.cv_image, (0,10,150), (110,110,255))
+        #self.hsv_image = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2HSV)
+        #self.binary_image2 = cv2.inRange(self.hsv_image, (5, 0, 0), (15, 360, 360))
+    def process_bump(self, msg):
+        if msg.leftFront or msg.rightFront:
+            self.twist = Twist(linear=Vector3(.3,0,0), angular=Vector3())
+            rospy.rate(1).sleep()
+        
+    def dribble(self):
+        moments = cv2.moments(self.binary_image)
+        if moments['m00'] != 0:
+            self.center_x, self.center_y = moments['m10']/moments['m00'], moments['m01']/moments['m00']
+        #normalize x based on the width of our image to range from -0.5 to 0.5
+        self.center_x = self.center_x * 1.0/640 - 0.5
+        forward = Vector3(0.0, 0.0, 0.0)
+        if (self.center_x < 0.25 and self.center_x > -0.25):
+            forward = Vector3(0.3, 0.0, 0.0)
 
+        self.twist = Twist(linear=forward, angular=Vector3(0.0, 0.0, -2 * self.center_x))
+        self.pub.publish(self.twist)
+
+    def stop(self):
+        """ This function publishes a twist to make the robot stop."""
+        self.pub.publish(Twist(linear=Vector3(0.0, 0.0, 0.0), angular=Vector3(0.0, 0.0, 0.0)))
+
+        
     def run(self):
         """ The main run loop, in this node it doesn't do anything """
         r = rospy.Rate(5)
+        rospy.on_shutdown(self.stop)
         while not rospy.is_shutdown():
             if not self.cv_image is None:
                 print self.cv_image.shape
-                cv2.imshow('video_window', self.cv_image)
+                cv2.imshow('video_window', self.binary_image)
+                cv2.imshow('window_2', self.cv_image)
+                #cv2.imshow('window_3', self.binary_image2)
                 cv2.waitKey(5)
-
-            # start out not issuing any motor commands
+                self.dribble()
+                
             r.sleep()
 
 if __name__ == '__main__':
